07df8745a73eca8a0264ad68ef141737131887a5
Leonardo Marquez <marqueztronics@gmail.com>
2024-03-28 20:26:42 -0300

    First commit - committing all files

---
+++ Debug/Drivers/Src/stm32f4xx_gpio.d
@@ -0,0 +1,7 @@
+Drivers/Src/stm32f4xx_gpio.o: ../Drivers/Src/stm32f4xx_gpio.c \
+ C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h \
+ C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h \
+ C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h
+C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h:
+C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h:
+C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h:
---
+++ README.md
@@ -0,0 +1,1 @@
+STM32F407 Timers Bare metal implementation.
