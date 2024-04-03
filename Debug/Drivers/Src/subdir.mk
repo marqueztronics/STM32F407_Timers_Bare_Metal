7adabbc4b1a892c746024abb29da4f3b51a7ce05
Leonardo Marquez <marqueztronics@gmail.com>
2024-04-03 11:01:49 -0300

    added .gitignore

--- Debug/Drivers/Src/subdir.mk
+++ Debug/Drivers/Src/subdir.mk
@@ -19,7 +19,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Drivers/Src/%.o Drivers/Src/%.su Drivers/Src/%.cyclo: ../Drivers/Src/%.c Drivers/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Drivers-2f-Src
 
--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,9 +1,9 @@
 Src/main.o: ../Src/main.c \
- C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h \
- C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h \
- C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h \
- C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gp_timer.h
-C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h:
-C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h:
-C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h:
-C:/Users/leona/Documents/Learning/STM32/F4/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gp_timer.h:
+ C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h \
+ C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h \
+ C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h \
+ C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gp_timer.h
+C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h:
+C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gpio.h:
+C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f407xx.h:
+C:/Users/leona/Documents/Learning/STM32/F4/F407/Timers_Bare_Metal/STM32F407_Timers_Bare_Metal/Drivers/Inc/stm32f4xx_gp_timer.h:
